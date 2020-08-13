cf90a87527afeaad4cffa5a2f21ab1bc6522bb8d
LeoChenL <leochenlang7@gmail.com>
2020-08-13 16:18:57 -0400

    Add init list example
    etc.
    etc.
    up code.
    help files.
    space, etc.
    doc, etc.

--- edu/Modern C++/High Performace/Constant Expressions/constExpressionCpp14.cpp
+++ edu/Modern C++/High Performace/Constant Expressions/constExpressionCpp14.cpp
@@ -19,9 +19,13 @@ int main(){
 
 /*
 Explanation #
-The difference between ordinary functions and constexpr functions in C++14 is minimal. It is quite easy to implement the gcd algorithm in C++14 as a constexpr function.
+The difference between ordinary functions and constexpr functions in C++14 is
+minimal. It is quite easy to implement the gcd algorithm in C++14 as a constexpr
+function.
 
-We have defined res as a constexpr variable, and its type is automatically determined by auto.
+We have defined res as a constexpr variable, and its type is automatically
+determined by auto.
 
-Note that we cannot use a non-constant value as input arguments for the gcd function.
+Note that we cannot use a non-constant value as input arguments for the gcd
+function.
 */
